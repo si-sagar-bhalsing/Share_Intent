4c039d013d05ba1173bb93a51053cb21680517ef
Sagar Bhalsing <140367558+si-sagar-bhalsing@users.noreply.github.com>
2024-06-07 16:22:56 +0530

    Minor changes

--- app/src/main/java/com/si/fanalytics/share_intent/ShareFunction.kt
+++ app/src/main/java/com/si/fanalytics/share_intent/ShareFunction.kt
@@ -3,8 +3,6 @@ package com.si.fanalytics.share_intent
 import android.content.Context
 import android.content.Intent
 import android.net.Uri
-import androidx.core.content.FileProvider
-import java.io.File
 
 fun shareImageAndText(context: Context, imageUri: Uri, text: String) {
     val shareIntent: Intent = Intent().apply {
